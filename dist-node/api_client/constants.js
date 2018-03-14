b62dea24651f1be1dccb032ab44c586201ceaece
tosch110 <tobias@lightcurve.io>
2018-03-14 10:25:58 +0100

    :construction_worker: Add build to the branch

---
+++ dist-node/api_client/constants.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/*
+ * Copyright © 2017 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+var GET = exports.GET = 'GET';
+var POST = exports.POST = 'POST';
+var PUT = exports.PUT = 'PUT';
